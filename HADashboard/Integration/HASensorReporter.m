ba4a0a810b248aae4bc85b047533e836323b3bba
Ash Hopkins <ashleighhopkins87@gmail.com>
2026-03-03 15:07:22 +0000

    refactor: remove per-sensor toggles, always report all sensors when registered
    Simplifies the Device Integration settings: one toggle to register,
    all 6 sensors report automatically when registered. No individual
    sensor on/off switches.
    
    Removed from Settings UI:
    - sensorsContainer, all 6 sensor UISwitch properties
    - addSensorToggleToStack: helper method
    - sensorToggleChanged: handler
    - objc_runtime import and associated object key
    - 6 NSUserDefaults sensor enable keys
    
    Removed from HASensorReporter:
    - isSensorEnabled: method and all NSUserDefaults key constants
    - Per-sensor enable/disable checks in registration and reporting
    
    Rule: registered = all sensors on. Not registered = nothing.

--- HADashboard/Integration/HASensorReporter.m
+++ HADashboard/Integration/HASensorReporter.m
@@ -11,16 +11,12 @@ static NSString *const kSensorStorage           = @"storage_available";
 static NSString *const kSensorAppState          = @"app_state";
 static NSString *const kSensorActiveDashboard   = @"active_dashboard";
 
-/// NSUserDefaults keys for per-sensor enable/disable (from Settings UI).
-static NSString *const kBatteryLevelEnabledKey    = @"ha_sensor_battery_level_enabled";
-static NSString *const kBatteryStateEnabledKey    = @"ha_sensor_battery_state_enabled";
-static NSString *const kScreenBrightnessEnabledKey = @"ha_sensor_screen_brightness_enabled";
-static NSString *const kStorageEnabledKey          = @"ha_sensor_storage_enabled";
-static NSString *const kAppStateEnabledKey         = @"ha_sensor_app_state_enabled";
-static NSString *const kActiveDashboardEnabledKey  = @"ha_sensor_active_dashboard_enabled";
-
-/// Notification name for dashboard changes (posted by HADashboardViewController).
 extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
+
+static NSArray *allSensorIds(void) {
+    return @[kSensorBatteryLevel, kSensorBatteryState, kSensorScreenBrightness,
+             kSensorStorage, kSensorAppState, kSensorActiveDashboard];
+}
 
 @interface HASensorReporter ()
 @property (nonatomic, assign) BOOL reporting;
@@ -33,107 +29,36 @@ extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
     [self stopReporting];
 }
 
-#pragma mark - Sensor Enable/Disable
-
-- (BOOL)isSensorEnabled:(NSString *)sensorId {
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    if ([sensorId isEqualToString:kSensorBatteryLevel]) {
-        // Default ON for battery level (most useful for kiosk)
-        return [defaults objectForKey:kBatteryLevelEnabledKey] == nil ? YES : [defaults boolForKey:kBatteryLevelEnabledKey];
-    }
-    if ([sensorId isEqualToString:kSensorBatteryState]) {
-        return [defaults boolForKey:kBatteryStateEnabledKey];
-    }
-    if ([sensorId isEqualToString:kSensorScreenBrightness]) {
-        return [defaults boolForKey:kScreenBrightnessEnabledKey];
-    }
-    if ([sensorId isEqualToString:kSensorStorage]) {
-        return [defaults boolForKey:kStorageEnabledKey];
-    }
-    if ([sensorId isEqualToString:kSensorAppState]) {
-        return [defaults boolForKey:kAppStateEnabledKey];
-    }
-    if ([sensorId isEqualToString:kSensorActiveDashboard]) {
-        return [defaults boolForKey:kActiveDashboardEnabledKey];
-    }
-    return NO;
-}
-
 #pragma mark - Sensor Registration
 
 - (void)registerSensors {
     HADeviceRegistration *reg = [HADeviceRegistration sharedManager];
     if (!reg.isRegistered) return;
 
-    if ([self isSensorEnabled:kSensorBatteryLevel]) {
-        [self registerSensorWithId:kSensorBatteryLevel
-                              name:@"Battery Level"
-                       deviceClass:@"battery"
-                 unitOfMeasurement:@"%"
-                        stateClass:@"measurement"
-                              icon:@"mdi:battery"];
-    }
-    if ([self isSensorEnabled:kSensorBatteryState]) {
-        [self registerSensorWithId:kSensorBatteryState
-                              name:@"Battery State"
-                       deviceClass:nil
-                 unitOfMeasurement:nil
-                        stateClass:nil
-                              icon:@"mdi:battery-charging"];
-    }
-    if ([self isSensorEnabled:kSensorScreenBrightness]) {
-        [self registerSensorWithId:kSensorScreenBrightness
-                              name:@"Screen Brightness"
-                       deviceClass:nil
-                 unitOfMeasurement:@"%"
-                        stateClass:@"measurement"
-                              icon:@"mdi:brightness-6"];
-    }
-    if ([self isSensorEnabled:kSensorStorage]) {
-        [self registerSensorWithId:kSensorStorage
-                              name:@"Storage Available"
-                       deviceClass:nil
-                 unitOfMeasurement:@"GB"
-                        stateClass:@"measurement"
-                              icon:@"mdi:harddisk"];
-    }
-    if ([self isSensorEnabled:kSensorAppState]) {
-        [self registerSensorWithId:kSensorAppState
-                              name:@"App State"
-                       deviceClass:nil
-                 unitOfMeasurement:nil
-                        stateClass:nil
-                              icon:@"mdi:application"];
-    }
-    if ([self isSensorEnabled:kSensorActiveDashboard]) {
-        [self registerSensorWithId:kSensorActiveDashboard
-                              name:@"Active Dashboard"
-                       deviceClass:nil
-                 unitOfMeasurement:nil
-                        stateClass:nil
-                              icon:@"mdi:view-dashboard"];
-    }
+    [self registerSensorWithId:kSensorBatteryLevel     name:@"Battery Level"     deviceClass:@"battery" unit:@"%" stateClass:@"measurement" icon:@"mdi:battery"];
+    [self registerSensorWithId:kSensorBatteryState     name:@"Battery State"     deviceClass:nil        unit:nil  stateClass:nil            icon:@"mdi:battery-charging"];
+    [self registerSensorWithId:kSensorScreenBrightness name:@"Screen Brightness" deviceClass:nil        unit:@"%" stateClass:@"measurement" icon:@"mdi:brightness-6"];
+    [self registerSensorWithId:kSensorStorage          name:@"Storage Available" deviceClass:nil        unit:@"GB" stateClass:@"measurement" icon:@"mdi:harddisk"];
+    [self registerSensorWithId:kSensorAppState         name:@"App State"         deviceClass:nil        unit:nil  stateClass:nil            icon:@"mdi:application"];
+    [self registerSensorWithId:kSensorActiveDashboard  name:@"Active Dashboard"  deviceClass:nil        unit:nil  stateClass:nil            icon:@"mdi:view-dashboard"];
 }
 
-- (void)registerSensorWithId:(NSString *)uniqueId
-                        name:(NSString *)name
-                 deviceClass:(NSString *)deviceClass
-           unitOfMeasurement:(NSString *)unit
-                  stateClass:(NSString *)stateClass
-                        icon:(NSString *)icon {
-    NSMutableDictionary *sensorData = [NSMutableDictionary dictionary];
-    sensorData[@"unique_id"] = uniqueId;
-    sensorData[@"name"] = name;
-    sensorData[@"type"] = @"sensor";
-    sensorData[@"entity_category"] = @"diagnostic";
-    if (deviceClass) sensorData[@"device_class"] = deviceClass;
-    if (unit)        sensorData[@"unit_of_measurement"] = unit;
-    if (stateClass)  sensorData[@"state_class"] = stateClass;
-    if (icon)        sensorData[@"icon"] = icon;
-    sensorData[@"state"] = [self currentValueForSensor:uniqueId];
+- (void)registerSensorWithId:(NSString *)uniqueId name:(NSString *)name
+                 deviceClass:(NSString *)deviceClass unit:(NSString *)unit
+                  stateClass:(NSString *)stateClass icon:(NSString *)icon {
+    NSMutableDictionary *data = [NSMutableDictionary dictionary];
+    data[@"unique_id"] = uniqueId;
+    data[@"name"] = name;
+    data[@"type"] = @"sensor";
+    data[@"entity_category"] = @"diagnostic";
+    if (deviceClass) data[@"device_class"] = deviceClass;
+    if (unit)        data[@"unit_of_measurement"] = unit;
+    if (stateClass)  data[@"state_class"] = stateClass;
+    if (icon)        data[@"icon"] = icon;
+    data[@"state"] = [self currentValueForSensor:uniqueId];
 
     [[HADeviceRegistration sharedManager] sendWebhookWithType:@"register_sensor"
-                                                         data:sensorData
+                                                         data:data
                                                    completion:^(id response, NSError *error) {
         if (error) {
             NSLog(@"[HASensorReporter] Failed to register sensor %@: %@", uniqueId, error.localizedDescription);
@@ -156,27 +81,21 @@ extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
                name:UIDeviceBatteryStateDidChangeNotification object:nil];
     [nc addObserver:self selector:@selector(brightnessDidChange:)
                name:UIScreenBrightnessDidChangeNotification object:nil];
-
-    // App state notifications
     [nc addObserver:self selector:@selector(appStateDidChange:)
                name:UIApplicationDidBecomeActiveNotification object:nil];
     [nc addObserver:self selector:@selector(appStateDidChange:)
                name:UIApplicationWillResignActiveNotification object:nil];
     [nc addObserver:self selector:@selector(appStateDidChange:)
                name:UIApplicationDidEnterBackgroundNotification object:nil];
-
-    // Active dashboard changes
     [nc addObserver:self selector:@selector(dashboardDidChange:)
                name:HAConnectionManagerDidReceiveLovelaceNotification object:nil];
 
     // Storage timer — every 15 minutes
-    if ([self isSensorEnabled:kSensorStorage]) {
-        self.storageTimer = [NSTimer scheduledTimerWithTimeInterval:900.0
-                                                             target:self
-                                                           selector:@selector(storageTimerFired:)
-                                                           userInfo:nil
-                                                            repeats:YES];
-    }
+    self.storageTimer = [NSTimer scheduledTimerWithTimeInterval:900.0
+                                                         target:self
+                                                       selector:@selector(storageTimerFired:)
+                                                       userInfo:nil
+                                                        repeats:YES];
 
     [self reportAllSensorsNow];
 }
@@ -185,67 +104,37 @@ extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
     if (!self.reporting) return;
     self.reporting = NO;
 
-    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-    [nc removeObserver:self name:UIDeviceBatteryLevelDidChangeNotification object:nil];
-    [nc removeObserver:self name:UIDeviceBatteryStateDidChangeNotification object:nil];
-    [nc removeObserver:self name:UIScreenBrightnessDidChangeNotification object:nil];
-    [nc removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
-    [nc removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
-    [nc removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
-    [nc removeObserver:self name:HAConnectionManagerDidReceiveLovelaceNotification object:nil];
-
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     [self.storageTimer invalidate];
     self.storageTimer = nil;
 }
 
 #pragma mark - Notification Handlers
 
-- (void)batteryLevelDidChange:(NSNotification *)note {
-    if ([self isSensorEnabled:kSensorBatteryLevel]) [self reportSensor:kSensorBatteryLevel];
-}
-
-- (void)batteryStateDidChange:(NSNotification *)note {
-    if ([self isSensorEnabled:kSensorBatteryState]) [self reportSensor:kSensorBatteryState];
-}
-
-- (void)brightnessDidChange:(NSNotification *)note {
-    if ([self isSensorEnabled:kSensorScreenBrightness]) [self reportSensor:kSensorScreenBrightness];
-}
-
-- (void)appStateDidChange:(NSNotification *)note {
-    if ([self isSensorEnabled:kSensorAppState]) [self reportSensor:kSensorAppState];
-}
-
-- (void)dashboardDidChange:(NSNotification *)note {
-    if ([self isSensorEnabled:kSensorActiveDashboard]) [self reportSensor:kSensorActiveDashboard];
-}
-
-- (void)storageTimerFired:(NSTimer *)timer {
-    if ([self isSensorEnabled:kSensorStorage]) [self reportSensor:kSensorStorage];
-}
+- (void)batteryLevelDidChange:(NSNotification *)note     { [self reportSensor:kSensorBatteryLevel]; }
+- (void)batteryStateDidChange:(NSNotification *)note     { [self reportSensor:kSensorBatteryState]; }
+- (void)brightnessDidChange:(NSNotification *)note       { [self reportSensor:kSensorScreenBrightness]; }
+- (void)appStateDidChange:(NSNotification *)note         { [self reportSensor:kSensorAppState]; }
+- (void)dashboardDidChange:(NSNotification *)note        { [self reportSensor:kSensorActiveDashboard]; }
+- (void)storageTimerFired:(NSTimer *)timer               { [self reportSensor:kSensorStorage]; }
 
 #pragma mark - Reporting
 
 - (void)reportAllSensorsNow {
     if (![HADeviceRegistration sharedManager].isRegistered) return;
 
-    NSArray *allSensors = @[kSensorBatteryLevel, kSensorBatteryState, kSensorScreenBrightness,
-                            kSensorStorage, kSensorAppState, kSensorActiveDashboard];
-    NSMutableArray *sensorData = [NSMutableArray array];
-    for (NSString *sensorId in allSensors) {
-        if ([self isSensorEnabled:sensorId]) {
-            [sensorData addObject:@{
-                @"unique_id": sensorId,
-                @"type": @"sensor",
-                @"state": [self currentValueForSensor:sensorId],
-                @"icon": [self iconForSensor:sensorId],
-            }];
-        }
+    NSMutableArray *batch = [NSMutableArray array];
+    for (NSString *sensorId in allSensorIds()) {
+        [batch addObject:@{
+            @"unique_id": sensorId,
+            @"type": @"sensor",
+            @"state": [self currentValueForSensor:sensorId],
+            @"icon": [self iconForSensor:sensorId],
+        }];
     }
-    if (sensorData.count == 0) return;
 
     [[HADeviceRegistration sharedManager] sendWebhookWithType:@"update_sensor_states"
-                                                         data:sensorData
+                                                         data:batch
                                                    completion:^(id response, NSError *error) {
         if (error) {
             NSLog(@"[HASensorReporter] Batch update error: %@", error.localizedDescription);
@@ -287,7 +176,7 @@ extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
 - (id)currentValueForSensor:(NSString *)sensorId {
     if ([sensorId isEqualToString:kSensorBatteryLevel]) {
         float level = [UIDevice currentDevice].batteryLevel;
-        if (level < 0) return @(-1); // Unknown
+        if (level < 0) return @(-1);
         return @((NSInteger)(level * 100));
     }
     if ([sensorId isEqualToString:kSensorBatteryState]) {
@@ -299,22 +188,19 @@ extern NSString *const HAConnectionManagerDidReceiveLovelaceNotification;
         }
     }
     if ([sensorId isEqualToString:kSensorScreenBrightness]) {
-        CGFloat brightness = [UIScreen mainScreen].brightness;
-        return @((NSInteger)(brightness * 100));
+        return @((NSInteger)([UIScreen mainScreen].brightness * 100));
     }
     if ([sensorId isEqualToString:kSensorStorage]) {
-        NSError *error = nil;
-        NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:&error];
+        NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:nil];
         if (attrs) {
             unsigned long long freeBytes = [attrs[NSFileSystemFreeSize] unsignedLongLongValue];
             double freeGB = freeBytes / (1024.0 * 1024.0 * 1024.0);
-            return @(round(freeGB * 10.0) / 10.0); // 1 decimal place
+            return @(round(freeGB * 10.0) / 10.0);
         }
         return @(-1);
     }
     if ([sensorId isEqualToString:kSensorAppState]) {
-        UIApplicationState state = [UIApplication sharedApplication].applicationState;
-        switch (state) {
+        switch ([UIApplication sharedApplication].applicationState) {
             case UIApplicationStateActive:     return @"Active";
             case UIApplicationStateInactive:   return @"Inactive";
             case UIApplicationStateBackground: return @"Background";
